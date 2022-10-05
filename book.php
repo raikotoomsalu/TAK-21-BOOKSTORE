51884e4b7e17dd6db796f58b258e328df1c87eeb
raikotoomsalu <raiko.toomsalu@ametikool.ee>
2022-10-05 09:38:21 +0300

    update

--- book.php
+++ book.php
@@ -9,3 +9,25 @@ $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
 var_dump($book);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$id;?></title>
+</head>
+<body>
+    <h1><?=$book['title'];?> * Released in <?=$book ["release_date"]?> *  €<?=$book['price']?></h1>
+    <img src="<?=$book['cover_path']?>" alt="">
+    <h1><?=$book ["summary"]?></h1>
+    <h1>Stock- <?=$book ["stock_saldo"]?></h1>
+    <h1>Pages- <?=$book["pages"]?></h1>
+    <div>
+        <span><a href="edit_form.php?id=<?=$id;?>"></a></span>
+    </div>
+</body>
+</html>
